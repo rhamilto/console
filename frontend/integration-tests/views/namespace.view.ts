2c45a6b5659a78804a98cd7f803b635eea6e0047
Samuel Padgett <spadgett@redhat.com>
2019-06-27 09:46:28 -0400

    Fix crud scenario namespace selector check

--- frontend/integration-tests/views/namespace.view.ts
+++ frontend/integration-tests/views/namespace.view.ts
@@ -1,5 +1,3 @@
-import { $, $$ } from 'protractor';
+import { $ } from 'protractor';
 
-export const namespaceBar = $('.co-namespace-bar');
-export const namespaceSelector = $('.co-namespace-selector');
-export const selectedNamespace = $$('.co-namespace-selector .dropdown .btn-link .btn-link__title').first();
+export const namespaceSelector = $('[data-test-id="namespace-bar-dropdown"]');
